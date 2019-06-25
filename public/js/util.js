f34b3ceea70568fc793c4942ea169a9da22f1d5e
小风 <48637636+zsf123@users.noreply.github.com>
2019-06-25 22:08:21 +0800

    Add files via upload

---
+++ public/js/util.js
@@ -0,0 +1,14 @@
+// 产生随机字符串
+function randomString(len) {
+　　len = len || 32;
+　　var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+　　var maxPos = $chars.length;
+　　var pwd = '';
+　　for (i = 0; i < len; i++) {
+　　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+　　}
+　　return pwd;
+}
+let strLength=randomString(32);
+
+module.exports=strLength;
